6c4510ac286aa8a51a55a70a9a20689fe553a53a
joschi127 <joschi127@users.noreply.github.com>
2018-05-19 02:18:39 +0200

    Fix startup, replace of document root in config was broken

--- 7.2.1-apache/init_container.sh
+++ 7.2.1-apache/init_container.sh
@@ -18,9 +18,12 @@ cat /etc/motd
 eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
 service ssh start
+APACHE_DOCUMENT_ROOT_ESCAPED="$(echo "$APACHE_DOCUMENT_ROOT" | sed "s/\//\\\\\//g")"
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
-sed -i "s/{APACHE_DOCUMENT_ROOT}/$APACHE_DOCUMENT_ROOT/g" /etc/apache2/apache2.conf
-sed -i "s/\/var\/www\/html/$APACHE_DOCUMENT_ROOT/g" /etc/apache2/sites-available/000-default.conf
+sed -i "s/{APACHE_DOCUMENT_ROOT}/$APACHE_DOCUMENT_ROOT_ESCAPED/g" /etc/apache2/apache2.conf
+sed -i "s/\/var\/www\/html/$APACHE_DOCUMENT_ROOT_ESCAPED/g" /etc/apache2/sites-available/000-default.conf
+mkdir -p "$APACHE_DOCUMENT_ROOT"
+chown $APACHE_RUN_USER "$APACHE_DOCUMENT_ROOT"
 mkdir /var/lock/apache2
 mkdir /var/run/apache2
 /usr/sbin/apache2ctl -D FOREGROUND
