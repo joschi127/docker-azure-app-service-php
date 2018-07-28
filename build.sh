7ae11c294e25db691fd99712bb430e776f968e09
joschi127 <joschi127@users.noreply.github.com>
2018-07-28 13:15:46 +0200

    Update build.sh script
    Do not pass password on command line
    Do not relogin if already logged in under correct username
    Use only one script for cached and uncached build

--- build.sh
+++ build.sh
@@ -1,46 +1,51 @@
 #!/usr/bin/env bash
-# arg1: name of destination dockerhub
-# arg2: dockerhub username
-# arg3: dockerhub password
-
 set -x -e
 
-if test "$1" = "" || test "$2" = "" || test "$3" = ""
+# parse arguments
+if [ "$1" = "" ] || ([ "$2" != "" ] && [ "$2" != "--no-cache" ])
 then
-    echo "Usage: $0 docker-hub-repository docker-hub-user docker-hub-password"
+    echo "Usage: $0 DOCKER_USERNAME [ --no-cache ]"
     exit 1
 fi
+DOCKER_USERNAME="$1"
+NO_CACHE_PARAM="$2"
 
-buildnumber=${4-$(date -u +"%y%m%d%H%M")}
+# login
+DOCKER_ALREADY_LOGGED_IN_USERNAME="$(docker info | grep 'Username:' | sed 's/Username: //')"
+if [ "$DOCKER_ALREADY_LOGGED_IN_USERNAME" = "" ] || [ "$DOCKER_ALREADY_LOGGED_IN_USERNAME" != "$DOCKER_USERNAME" ]
+then
+    docker login -u "$DOCKER_USERNAME"
+fi
 
-docker login -u "$2" -p "$3"
+# generate build number
+buildnumber=$(date -u +"%Y%m%d_%H%M")
 
 # build base images
-#docker build -q -t "$1"/azure-app-service-php:5.6.36-apache_"$buildnumber" 5.6.36-apache
-#docker build -q -t "$1"/azure-app-service-php:7.0.30-apache_"$buildnumber" 7.0.30-apache
-docker build -q -t "$1"/azure-app-service-php:7.2.5-apache_"$buildnumber" -t "$1"/azure-app-service-php:latest_"$buildnumber" 7.2.5-apache
-docker tag "$1"/azure-app-service-php:latest_"$buildnumber" "$1"/azure-app-service-php:latest
+#docker build $NO_CACHE_PARAM -q -t joschi127/azure-app-service-php:5.6.36-apache_"$buildnumber" 5.6.36-apache
+#docker build $NO_CACHE_PARAM -q -t joschi127/azure-app-service-php:7.0.30-apache_"$buildnumber" 7.0.30-apache
+docker build $NO_CACHE_PARAM -q -t joschi127/azure-app-service-php:7.2.5-apache_"$buildnumber" -t joschi127/azure-app-service-php:latest_"$buildnumber" 7.2.5-apache
+docker tag joschi127/azure-app-service-php:latest_"$buildnumber" joschi127/azure-app-service-php:latest
 
-#docker push "$1"/azure-app-service-php:5.6.36-apache_"$buildnumber"
-#docker push "$1"/azure-app-service-php:7.0.30-apache_"$buildnumber"
-docker push "$1"/azure-app-service-php:7.2.5-apache_"$buildnumber"
-docker push "$1"/azure-app-service-php:latest_"$buildnumber"
-docker push "$1"/azure-app-service-php:latest
+#docker push joschi127/azure-app-service-php:5.6.36-apache_"$buildnumber"
+#docker push joschi127/azure-app-service-php:7.0.30-apache_"$buildnumber"
+docker push joschi127/azure-app-service-php:7.2.5-apache_"$buildnumber"
+docker push joschi127/azure-app-service-php:latest_"$buildnumber"
+docker push joschi127/azure-app-service-php:latest
 
 # xdebug depends on base images
 # generate dockerfile for xdebug
-#sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 5.6.36-apache-xdebug/Dockerfile.template > 5.6.36-apache-xdebug/Dockerfile
-#sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 7.0.30-apache-xdebug/Dockerfile.template > 7.0.30-apache-xdebug/Dockerfile
-#sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 7.2.5-apache-xdebug/Dockerfile.template > 7.2.5-apache-xdebug/Dockerfile
+#sed -e s/reponame/joschi127/g -e s/buildnumber/"$buildnumber"/g 5.6.36-apache-xdebug/Dockerfile.template > 5.6.36-apache-xdebug/Dockerfile
+#sed -e s/reponame/joschi127/g -e s/buildnumber/"$buildnumber"/g 7.0.30-apache-xdebug/Dockerfile.template > 7.0.30-apache-xdebug/Dockerfile
+#sed -e s/reponame/joschi127/g -e s/buildnumber/"$buildnumber"/g 7.2.5-apache-xdebug/Dockerfile.template > 7.2.5-apache-xdebug/Dockerfile
 
 # build xdebug images
-#docker build -q -t "$1"/azure-app-service-php:5.6.36-apache-xdebug_"$buildnumber" 5.6.36-apache-xdebug
-#docker build -q -t "$1"/azure-app-service-php:7.0.30-apache-xdebug_"$buildnumber" 7.0.30-apache-xdebug
-#docker build -q -t "$1"/azure-app-service-php:7.2.5-apache-xdebug_"$buildnumber" -t "$1"/azure-app-service-php:latest-xdebug_"$buildnumber" 7.2.5-apache-xdebug
+#docker build -q -t joschi127/azure-app-service-php:5.6.36-apache-xdebug_"$buildnumber" 5.6.36-apache-xdebug
+#docker build -q -t joschi127/azure-app-service-php:7.0.30-apache-xdebug_"$buildnumber" 7.0.30-apache-xdebug
+#docker build -q -t joschi127/azure-app-service-php:7.2.5-apache-xdebug_"$buildnumber" -t joschi127/azure-app-service-php:latest-xdebug_"$buildnumber" 7.2.5-apache-xdebug
 
-#docker push "$1"/azure-app-service-php:5.6.36-apache-xdebug_"$buildnumber"
-#docker push "$1"/azure-app-service-php:7.0.30-apache-xdebug_"$buildnumber"
-#docker push "$1"/azure-app-service-php:7.2.5-apache-xdebug_"$buildnumber"
-#docker push "$1"/azure-app-service-php:latest-xdebug_"$buildnumber"
+#docker push joschi127/azure-app-service-php:5.6.36-apache-xdebug_"$buildnumber"
+#docker push joschi127/azure-app-service-php:7.0.30-apache-xdebug_"$buildnumber"
+#docker push joschi127/azure-app-service-php:7.2.5-apache-xdebug_"$buildnumber"
+#docker push joschi127/azure-app-service-php:latest-xdebug_"$buildnumber"
 
 docker logout
